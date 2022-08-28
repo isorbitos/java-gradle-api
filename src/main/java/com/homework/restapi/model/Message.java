3f3986c5cf77788d08e673f6b4e502f5d81f7e56
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2022-08-28 20:54:02 +0300

    main tables added, swagger added

--- src/main/java/com/homework/restapi/model/Message.java
+++ src/main/java/com/homework/restapi/model/Message.java
@@ -1,4 +1,21 @@
 package com.homework.restapi.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name="Messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String msgText;
+    private Timestamp msgCreated;
+
+    @ManyToOne
+    private User user;
+
 }
--- src/main/java/com/homework/restapi/model/User.java
+++ src/main/java/com/homework/restapi/model/User.java
@@ -1,4 +1,26 @@
 package com.homework.restapi.model;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(name = "user_id")
+    private List<Message> messages = new ArrayList<>();
+
 }
